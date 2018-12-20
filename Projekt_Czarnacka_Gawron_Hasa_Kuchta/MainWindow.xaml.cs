4004712a30a82f8860adb24e70a05c40eb3c2ca3
Kucheu <kuchcik1920@gmail.com>
2018-12-20 22:49:19 +0100

    Logowanie

--- Projekt_Czarnacka_Gawron_Hasa_Kuchta/MainWindow.xaml.cs
+++ Projekt_Czarnacka_Gawron_Hasa_Kuchta/MainWindow.xaml.cs
@@ -29,43 +29,71 @@ namespace Projekt_Czarnacka_Gawron_Hasa_Kuchta
 
         private void LoginBtn_Click(object sender, RoutedEventArgs e)
         {
-            if (userTxt.Text == "r")
+            try
             {
                 connString = new SqlConnectionStringBuilder();
                 connString.InitialCatalog = "Firma";
-                connString.DataSource = "DESKTOP-70K18SL\\SQLEXPRESS";
+                // connString.DataSource = "DESKTOP-70K18SL\\SQLEXPRESS";
+                connString.DataSource = "localhost";
                 connString.IntegratedSecurity = true;
                 conn = new SqlConnection(connString.ConnectionString);
                 conn.Open();
-                Recepcja recepcjaOkno = new Recepcja(conn, userTxt.Text);
-                recepcjaOkno.Show();
-                this.Close();
+
+                SqlCommand query = new SqlCommand();
+                string polecenie = "Select Stanowisko from Pracownicy where login=@login and password=@password"; 
+                query.Parameters.AddWithValue("@login", userTxt.Text); 
+                query.Parameters.AddWithValue("@password", passwordTxt.Password);
+                query.CommandText = polecenie;
+                query.Connection = conn;
+
+                SqlDataReader reader = query.ExecuteReader();
+
+                int ilosc = 0;
+                string stanowisko = "";
+
+                while (reader.Read())
+                {
+                    ilosc++;
+                    stanowisko = reader.GetString(0).Replace(" ", "");
+                }
+                reader.Close();
+
+                if (ilosc == 1)
+                {
+                    if (stanowisko == "Recepcjonistka")
+                    {
+                        Recepcja recepcjaOkno = new Recepcja(conn, userTxt.Text);
+                        recepcjaOkno.Show();
+                        this.Close();
+                    }
+                    else if (stanowisko == "Lekarz")
+                    {
+                        Lekarz lekarzOkno = new Lekarz();
+                        lekarzOkno.Show();
+                        this.Close();
+                    }
+                    else if (stanowisko == "Wlasciciel")
+                    {
+                        Wlasciciel bossOkno = new Wlasciciel();
+                        bossOkno.Show();
+                        this.Close();
+                    }
+                    else
+                    {
+                        conn.Close();
+                        
+                    }
+                }
+
+                userTxt.Clear();
+                passwordTxt.Clear();
             }
-            else if (userTxt.Text == "l")
+            catch(SqlException)
             {
-                connString = new SqlConnectionStringBuilder();
-                connString.InitialCatalog = "Firma";
-                connString.DataSource = "DESKTOP-70K18SL\\SQLEXPRESS";
-                connString.IntegratedSecurity = true;
-                conn = new SqlConnection(connString.ConnectionString);
-                conn.Open();
-                Lekarz lekarzOkno = new Lekarz();
-                lekarzOkno.Show();
-                this.Close();
+                // cos co sie stanie przy blencie
             }
-            else if (userTxt.Text == "w")
-            {
-                connString = new SqlConnectionStringBuilder();
-                connString.InitialCatalog = "Firma";
-                connString.DataSource = "DESKTOP-70K18SL\\SQLEXPRESS";
-                connString.IntegratedSecurity = true;
-                conn = new SqlConnection(connString.ConnectionString);
-                conn.Open();
-                Wlasciciel bossOkno = new Wlasciciel();
-                bossOkno.Show();
-                this.Close();
-                
-            }
+            
+            
             
 
         }
